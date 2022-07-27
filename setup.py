a36d39334ed6063cf10489f0873deacea950d989
John Sturgeon <john.sturgeon@me.com>
2022-07-26 18:10:09 -0700

    Checked in module

---
+++ setup.py
@@ -0,0 +1,23 @@
+""" Setup file for module """
+from setuptools import setup
+
+with open('README.md', encoding='utf8') as f:
+    long_description = f.read()
+
+with open('requirements.txt', encoding='utf8') as f:
+    requirements = f.readlines()
+
+setup(
+    name='yahoo-nfl',
+    version='0.0.1',
+    packages=['yahoo_nfl'],
+    python_requires='>=3.10.*',
+    url='https://github.com/johnsturgeon/yahoo-nfl',
+    license='MIT',
+    author='John Sturgeon',
+    author_email='john.sturgeon@me.com',
+    install_requires=requirements,
+    description='Python extraction of NFL scores schedule data from Yahoo',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)

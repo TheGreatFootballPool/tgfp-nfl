d3b581f6d2b2dd1b7608ae62e99450e607144295
John Sturgeon <john.sturgeon@me.com>
2023-09-19 09:36:34 -0700

    Updated poetry environment

--- bump_version_and_publish.sh
+++ bump_version_and_publish.sh
@@ -8,4 +8,4 @@ NEW_VERSION=`poetry version -s`
 git tag v${NEW_VERSION}
 git push origin v${NEW_VERSION}
 PYPI_TOKEN=`echo "op://Private/pypi.org/api_token" | op inject`
-poetry publish --build --username __token__ --password PYPI_TOKEN
+poetry publish --build --username __token__ --password ${PYPI_TOKEN}
